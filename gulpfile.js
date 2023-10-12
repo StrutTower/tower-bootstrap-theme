3a3e0e253804e7897637918950e4aefe4b3fe861
Alex Tiller <StrutTower@outlook.com>
2023-10-11 17:02:07 -0700

    Starting to switch to Bootstrap 5.3's new theme settings

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,25 @@ var gulp = require('gulp')
 
 var options = {
     css: {
-        light: {
-            sassFile: 'src/light-theme.scss',
-            output: 'tower-light-theme.css'
-        },
-        dark: {
-            sassFile: 'src/dark-theme.scss',
-            output: 'tower-dark-theme.css'
-        },
+        sassMainFile: 'src/bootstrap.scss',
+        output: 'tower-bootstrap-theme.css',
         sassFiles: 'src/**/*.scss',
         dest: 'dist'
     }
 };
 
-gulp.task('light-theme-css', function () {
-    return gulp.src(options.css.light.sassFile)
+gulp.task('build-css', function () {
+    return gulp.src(options.css.sassMainFile)
         .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
-        .pipe(concat(options.css.light.output))
+        .pipe(concat(options.css.output))
         .pipe(gulp.dest(options.css.dest))
         .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(options.css.dest));
-});
-
-gulp.task('dark-theme-css', function () {
-    return gulp.src(options.css.dark.sassFile)
-        .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
-        .pipe(concat(options.css.dark.output))
-        .pipe(gulp.dest(options.css.dest))
-        .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(options.css.dest));
-});
+})
 
 gulp.task('watch-sass', function () {
-    return gulp.watch(options.css.sassFiles, gulp.parallel(['light-theme-css', 'dark-theme-css']));
+    return gulp.watch(options.css.sassFiles, gulp.parallel(['build-css']));
 });
 
-gulp.task('default', gulp.parallel(['light-theme-css', 'dark-theme-css']));
+gulp.task('default', gulp.parallel(['build-css']));
