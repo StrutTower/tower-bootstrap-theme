259da4bb82215152084a6f48972347526a3e1231
StrutTower <StrutTower@gmail.com>
2023-10-26 17:57:16 -0700

    Fixed border radius settings Fixed navbar toggler Updated demo site

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-﻿/// <binding ProjectOpened='watch-sass' />
+/// <binding ProjectOpened='watch-sass, watch-html' />
 var gulp = require('gulp')
     sass = require('gulp-dart-sass')
     cleancss = require('gulp-clean-css')
     concat = require('gulp-concat')
-    rename = require('gulp-rename');
+    rename = require('gulp-rename')
+    fileInclude = require('gulp-file-include')
+    htmlBeautify = require('gulp-html-beautify');
 
 var options = {
     css: {
@@ -13,6 +15,11 @@ var options = {
         output2: 'tower-bootstrap-theme-media-query.css',
         sassFiles: 'src/**/*.scss',
         dest: 'dist'
+    },
+    html: {
+        files: 'demo-templates/*.html',
+        watchFiles: 'demo-templates/**/*.html',
+        dest: 'demo'
     }
 };
 
@@ -21,7 +28,7 @@ gulp.task('build-css', function () {
         .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
         .pipe(concat(options.css.output))
         .pipe(gulp.dest(options.css.dest))
-        .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
+        .pipe(cleancss())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(options.css.dest));
 });
@@ -31,13 +38,24 @@ gulp.task('build-css-media-query', function () {
         .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
         .pipe(concat(options.css.output2))
         .pipe(gulp.dest(options.css.dest))
-        .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
+        .pipe(cleancss())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(options.css.dest));
+});
+
+gulp.task('build-html', function () {
+    return gulp.src(options.html.files)
+        .pipe(fileInclude())
+        .pipe(htmlBeautify())
+        .pipe(gulp.dest(options.html.dest));
 });
 
 gulp.task('watch-sass', function () {
     return gulp.watch(options.css.sassFiles, gulp.parallel(['build-css']));
 });
+
+gulp.task('watch-html', function () {
+    return gulp.watch(options.html.watchFiles, gulp.parallel(['build-html']));
+})
 
 gulp.task('default', gulp.parallel(['build-css', 'build-css-media-query']));
